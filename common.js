0ca5cc79686d65df97be449c45a6d1f552e2dfd4
mounika-wal <mounika.j@westagilelabs.com>
2018-05-29 10:09:20 +0530

    File name changes

--- common.js
+++ common.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('#main_container').load('signup_header_component/signup-header.html');
+    $('#main_container').load('login_header_component/login-header.html');
     // $('#main_container').load('about_us_component/about-us.html');
     var location_href = window.location.href;
     var result = location_href.substring(location_href.lastIndexOf('/')+1);
