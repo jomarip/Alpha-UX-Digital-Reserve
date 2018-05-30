ddc573d5b4b3f46c1dd74a2882be3d3e9927d082
mohan-wal <mohan.k@westagilelabs.com>
2018-05-30 14:09:00 +0530

    Forgot Password UI completed for Desktop & Mobile view as well

--- unsigned_header_component/login_component/login.js
+++ unsigned_header_component/login_component/login.js
@@ -2,4 +2,13 @@ $(document).ready(function() {
     $('#dark-close-icon, .backdrop').on('click', () =>{
         $('#loginSection').removeClass('login-open');
     })
+
+    $('#forgotPwd').on('click', () => {
+        $('#loginSection').removeClass('login-open');
+        $('#loginSection').addClass('forgotpwd-open');
+    })
+
+    $('.darkpwd-close-icon, .backdrop').on('click', () =>{
+        $('#loginSection').removeClass('forgotpwd-open');
+    })
 });
