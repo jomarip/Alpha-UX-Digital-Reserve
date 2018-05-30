8c3a8e7c2923696dd0632575e7124e14b019ccc3
mohan-wal <mohan.k@westagilelabs.com>
2018-05-30 17:37:11 +0530

    Completed Sign-Up screen for both Desktop & Mobile View

--- unsigned_header_component/unsigned-header.js
+++ unsigned_header_component/unsigned-header.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
     $('#loginSection').load('unsigned_header_component/login_component/login.html');
+    $('.revolution-btn').on('click', () => {
+        $('#loginSection').addClass('signup-open');
+    });
     $('.login-btn').on('click', () => {
         $('#loginSection').addClass('login-open');
     })
