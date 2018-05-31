96511ff10d153ef2015f0946136bc7a06561b108
mohan-wal <mohan.k@westagilelabs.com>
2018-05-31 14:02:12 +0530

    Completed Shared Details Design

---
+++ unsigned_header_component/share_details_component/share-details.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    //Shared Details page functionality
+    $('.darkshare-details-close-icon, .backdrop').on('click', () =>{
+        $('#shareDetailsSection').removeClass('share-details-open');
+    })
+});
