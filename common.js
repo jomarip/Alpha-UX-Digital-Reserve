704abf13c98072561200684c8a78c66e93394751
mounika-wal <mounika.j@westagilelabs.com>
2018-05-31 18:24:22 +0530

    Created Contact-Us-popup

--- common.js
+++ common.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
     $('#main-container').load('unsigned_header_component/unsigned-header.html');
     $('#footer-container').load('footer_component/footer.html');
     $('#switch-node-popup-container').load('switch_node_popup_component/switch-node-popup.html');
-    $('#lite-full-node-popup-container').load('lite_full_switch_node_popup_component/lite-full-switch-node-popup.html')
+    $('#lite-full-node-popup-container').load('lite_full_switch_node_popup_component/lite-full-switch-node-popup.html');
+    $('#contact-us-popup-container').load('contact_us_popup_component/contact-us-popup.html')
     // $('#main_container').load('about_us_component/about-us.html');
     var location_href = window.location.href;
     var result = location_href.substring(location_href.lastIndexOf('/')+1);
@@ -14,12 +15,10 @@ $(document).ready(function() {
     }
 
     $(document).on('click', '.toggle-icon', function(){
-            $('.header-wrapper').toggleClass('show-menu');
-
+        $('.header-wrapper').toggleClass('show-menu');
     })
     $(document).on('click', '.close-icon', function(){
         $('.header-wrapper').removeClass('show-menu',500);
     })
-
-    
+   
 });
