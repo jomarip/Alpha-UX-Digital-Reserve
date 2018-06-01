57734a382f49a30db7a7d2c171ffc75ad2938d98
mounika-wal <mounika.j@westagilelabs.com>
2018-06-01 12:19:14 +0530

    created abou-us

---
+++ about_us_component/about-us.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function() {
+    alert();
+    $('#about-us-header-container').load('unsigned_header_component/unsigned-header.html');
+    $('#about-us-footer-container').load('footer_component/footer.html');
+});
