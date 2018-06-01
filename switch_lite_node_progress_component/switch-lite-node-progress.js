b2d634a64327f92c5014f728a450657f59433136
mounika-wal <mounika.j@westagilelabs.com>
2018-06-01 17:28:20 +0530

    created about-us page

---
+++ switch_lite_node_progress_component/switch-lite-node-progress.js
@@ -0,0 +1,9 @@
+$(document).on('click', '.switch-node-progress-block', function(){
+    alert('kjsdngkjs');
+    $('.progress-bar-loading').animate(
+        {width:'100%'}, 
+        {
+            duration:5000     
+        }    
+    )
+});
