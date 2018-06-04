02713061dd1e2f3ded519eacb24882363d9db9cc
mounika-wal <mounika.j@westagilelabs.com>
2018-06-04 15:31:47 +0530

    Minor css changes

--- about_us_component/about-us.js
+++ about_us_component/about-us.js
@@ -1,6 +1,5 @@
 
-$(document).ready(function() {
-    alert();
-    $('#about-us-header-container').load('unsigned_header_component/unsigned-header.html');
-    $('#about-us-footer-container').load('footer_component/footer.html');
-});
+// $(document).ready(function() {
+//     $('#about-us-header-container').load('unsigned_header_component/unsigned-header.html');
+//     $('#about-us-footer-container').load('footer_component/footer.html');
+// });
