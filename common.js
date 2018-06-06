40ed070b3dae13426742fd545bbd402ead4b4037
mounika-wal <mounika.j@westagilelabs.com>
2018-06-06 17:55:58 +0530

    Created Signed header

--- common.js
+++ common.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     $('#switch-node-popup-container').load('switch_node_popup_component/switch-node-popup.html');
     $('#lite-full-node-popup-container').load('lite_full_switch_node_popup_component/lite-full-switch-node-popup.html');
     $('#contact-us-popup-container').load('contact_us_popup_component/contact-us-popup.html');
-
+    $('#signed-header-container').load('signed_header_component/signed-header.html')
     // $('#main_container').load('about_us_component/about-us.html');
     var location_href = window.location.href;
     var result = location_href.substring(location_href.lastIndexOf('/')+1);
@@ -21,6 +21,14 @@ $(document).ready(function() {
     })
     $(document).on('click', '.close-icon', function(){
         $('.header-wrapper').removeClass('show-menu',500);
+    })
+    $(document).on('click', '.signed-toggle-icon', function(){
+        $('.signed-collapse-wrapper').toggleClass('show-signed-menu');
+    })
+    $(document).on('click', '.menu-close-icon', function(){
+        $('.signed-collapse-wrapper').removeClass('show-signed-menu');
+        $('.signed-collapse-wrapper').removeClass('show');
+
     })
     $(document).on('click', '.list-link', function(){
         var attt_value = $(this).attr('data-attr');
