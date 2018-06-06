8ba08cda1767af4f36fb193df94b8bc24dcb8454
mounika-wal <mounika.j@westagilelabs.com>
2018-06-06 10:39:32 +0530

    UI-ISsues fixed

--- about_us_component/about-us.js
+++ about_us_component/about-us.js
@@ -1,5 +1,5 @@
 
 // $(document).ready(function() {
-//     $('#about-us-header-container').load('unsigned_header_component/unsigned-header.html');
-//     $('#about-us-footer-container').load('footer_component/footer.html');
+//     $('#about-us-header-container').load('about_us_component/unsigned_header_component/unsigned-header.html');
+//     $('#about-us-footer-container').load('about_us_component/footer_component/footer.html');
 // });
