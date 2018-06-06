dbf20dd02307c1b4a30eeb58b27e7492cfe7a4dd
mounika-wal <mounika.j@westagilelabs.com>
2018-06-06 18:56:23 +0530

    FAQ

--- common.js
+++ common.js
@@ -5,7 +5,9 @@ $(document).ready(function() {
     $('#switch-node-popup-container').load('switch_node_popup_component/switch-node-popup.html');
     $('#lite-full-node-popup-container').load('lite_full_switch_node_popup_component/lite-full-switch-node-popup.html');
     $('#contact-us-popup-container').load('contact_us_popup_component/contact-us-popup.html');
-    $('#signed-header-container').load('signed_header_component/signed-header.html')
+    // $('#signed-header-container').load('signed_header_component/signed-header.html')
+    // $('#home-container').load('home_component/home.html')
+    $('#faq-container').load('faq_component/faq.html');
     // $('#main_container').load('about_us_component/about-us.html');
     var location_href = window.location.href;
     var result = location_href.substring(location_href.lastIndexOf('/')+1);
@@ -30,20 +32,16 @@ $(document).ready(function() {
         $('.signed-collapse-wrapper').removeClass('show');
 
     })
-    $(document).on('click', '.list-link', function(){
-        var attt_value = $(this).attr('data-attr');
-        alert(attt_value);
-        if( attt_value == "Home"){
-            $('#home-container').load('home_component/home.html')
-        }
-        else if( attt_value == "AboutUs"){
-            $('#about-us-conatiner').load('about_us_component/about-us.html');
-        }
-        else if(attt_value == "FAQ"){
-            $('#faq-container').load('faq_component/faq.html');
-        }
-        else{
-            // $('#home-container').load('home_component/home.html')
-        }
-    })
+    // $(document).on('click', '.list-link', function(){
+    //     var attt_value = $(this).attr('data-attr');
+    //      if( attt_value == "AboutUs"){
+    //         $('#about-us-conatiner').load('about_us_component/about-us.html');
+    //     }
+    //     else if(attt_value == "FAQ"){
+    //         $('#faq-container').load('faq_component/faq.html');
+    //     }
+    //     else{
+
+    //     }
+    // })
 });
