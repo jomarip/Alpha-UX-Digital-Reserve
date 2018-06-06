b239c88f3230d46ca31b8397544dc1b3abf25eb3
mohan-wal <mohan.k@westagilelabs.com>
2018-06-06 12:28:20 +0530

    Updates for home page

--- common.js
+++ common.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
     $('#lite-full-node-popup-container').load('lite_full_switch_node_popup_component/lite-full-switch-node-popup.html');
     $('#contact-us-popup-container').load('contact_us_popup_component/contact-us-popup.html');
     $('#about-us-conatiner').load('about_us_component/about-us.html')
+    $('#home-container').load('home_component/home.html')
     // $('#main_container').load('about_us_component/about-us.html');
     var location_href = window.location.href;
     var result = location_href.substring(location_href.lastIndexOf('/')+1);
