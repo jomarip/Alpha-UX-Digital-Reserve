d8fbc31217ab8240a30633378f418d5abe8ba840
Sevanti Nag <sevanti@westagilelabs.com>
2018-06-08 18:51:00 +0530

    UI for dashboard mobile done

---
+++ dashboard_component/dashboard.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('#signed-header-container').load('../signed_header_component/signed-header.html');
+    $('#dashboard_detail_component').load('dashboard_detail_component/dashboard-detail.html');
+    $('#dashboard_favourites_component').load('dashboard_favourites_component/dashboard-favourites.html');
+    $('#dashboard_all_denarii_component').load('dashboard_all_denarii_component/dashboard-all-denarii.html');
+    $('#dashboard_recent_transactions_component').load('dashboard_recent_transactions_component/dashboard-recent-transactions.html');
+    $('#dashboard_minting_component').load('dashboard_minting_component/dashboard-minting.html');
+    
+    $(document).on('click', '.signed-toggle-icon', function(){
+        $('.signed-header-wrapper').toggleClass('show-signed-menu');
+    });
+
+    $(document).on('click', '.menu-close-icon', function(){
+        $('.signed-header-wrapper').removeClass('show-signed-menu');
+    });
+});
