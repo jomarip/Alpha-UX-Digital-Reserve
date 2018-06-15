23b2ae98952d2fc196a75221b1a4dd35746fa74b
mounika-wal <mounika.j@westagilelabs.com>
2018-06-15 07:47:59 +0530

    UI-issues fixed in the denarii

--- unsigned_header_component/share_details_component/share-details.js
+++ unsigned_header_component/share_details_component/share-details.js
@@ -3,4 +3,20 @@ $(document).ready(function() {
     $('.darkshare-details-close-icon, .backdrop').on('click', () =>{
         $('#shareDetailsSection').removeClass('share-details-open');
     })
+    $(".share-details-content input").focus(function(){
+        $(this).addClass("field-focus");
+        
+        }).blur(function(){
+        $(this).removeClass("field-focus");
+        $(this).next('.val-button').removeClass('update-value');
+        tmpval = $(this).val();
+        if(tmpval == '') {
+        if( $(this).hasClass('field-focus')){
+            $(this).removeClass('field-focus');
+        }
+        }
+        else {
+            $(this).addClass('field-focus');
+        }
+    });
 });
