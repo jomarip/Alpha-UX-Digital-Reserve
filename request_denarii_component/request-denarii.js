1e0e24a021f8ad3dde0a39a2b595113e658153f8
mounika-wal <mounika.j@westagilelabs.com>
2018-06-15 14:32:11 +0530

    changes in the denarii

--- request_denarii_component/request-denarii.js
+++ request_denarii_component/request-denarii.js
@@ -22,11 +22,11 @@ $(document).ready(function($) {
        var input_value = this.value;
        $(document).on('click','.increment-icon', function(){
             var inc_value = ++input_value;
-            $(".request-denarii-value").val(inc_value);
+            $(".request-denarii-value").val('$'  + inc_value);
        })
        $(document).on('click','.decrement-icon', function(){
             var dec_value = --input_value;
-            $(".request-denarii-value").val(dec_value);
+            $(".request-denarii-value").val('$' + dec_value);
         })
       });
    });
