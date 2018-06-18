97c35867a05e5affa82182cb6f355f41ae19c380
Sevanti Nag <sevanti@westagilelabs.com>
2018-06-18 15:13:32 +0530

    fix for progress bar

--- dashboard_component/dashboard_minting_component/dashboard-minting.js
+++ dashboard_component/dashboard_minting_component/dashboard-minting.js
@@ -1,4 +1,32 @@
-$(document).ready(function() {
-
-
-});
+window.onload = function onLoad() {
+  setTimeout(function(){
+    var semiBar = new ProgressBar.SemiCircle("#minting-progress-container", {
+        strokeWidth: 7,
+        trailWidth: 7,
+        trailColor: "#e6e6e6",
+        easing: "bounce",
+        from: { color: "#3f9dfe", width: 7 },
+        to: { color: "#3f9dfe", width: 7 },
+        text: {
+          value: '0',
+          className: 'progress-text',
+          style: {
+            position: 'absolute',
+            top: '51px',
+            left: '50%',
+            padding: 0,
+            margin: 0,
+            transform: null
+          }
+        },
+        step: (state, shape, attachment) => {
+          shape.path.setAttribute("stroke", state.color);
+          shape.path.setAttribute("stroke-width", state.width);
+          shape.setText('<span class="progress-value">'+Math.round(shape.value() * 100)+'</span> <span class="progress-percent">%</span>');
+        }
+      });
+      semiBar.animate(0.75, {
+        duration: 2000
+      });
+    }, 3000)
+};
